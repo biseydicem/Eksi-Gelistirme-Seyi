969da9eae5be9085a399be9727a2df4cd3b8576f
biseydicem <biseydicemben@gmail.com>
2016-12-26 17:51:43 +0300

    Update için versiyon numarası arttırıldı

--- egs.user.js
+++ egs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Ekşi Geliştirme Şeyi
 // @description    sanatsal dokunuşlarla ekşi sözlük'ü güzelleştirir
-// @version        4
+// @version        5
 // @author         biseydicem
 // @copyright      2016, bi sey dicem
 // @license        GPL v3 or later; http://www.gnu.org/licenses/gpl.html
